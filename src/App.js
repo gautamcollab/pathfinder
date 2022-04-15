ba22be419daf6bfb6642e4b0c975826923e02298
Gautam Sinha <gautam.sinha@live.com>
2022-04-15 18:04:29 -0400

    created app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 
 function App() {
  
+// //const container = document.getElementById('container')
+
+// const pathfinder = async () => {
+//   let req = await fetch('https://api.pathfinder2.fr/v1/pf2/spell',{
+//   headers:{Authorization:'da468b89-2bf8-4e2b-a939-79c6e6ef25ce'}
+//   }
+//   )
+//   let res = await req.json()
+//   res.results.forEach((element) => {
+//       let h2 = document.createElement('h2')
+//       h2.innerText = element.data.description
+//       container.append(h2)
+//       console.log(res.results)
+//   } 
+//   )
+  
+// }
+
+// pathfinder()
+
 }
 
 export default App;
