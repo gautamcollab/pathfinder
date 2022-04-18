1ccd61f454af401bc52d5fa3460f3a6eb28e86a6
John Kim <joki9448@gmail.com>
2022-04-18 14:21:50 -0400

    started background data fetch

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -1,9 +1,9 @@
-function Background() {
+function Background({ element }) {
     return (
         <div>
             <h1>Background</h1>
+            <p>{element}</p>
         </div>
     )
 }
-
 export default Background
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Class from './Class'
 import Background from './Background'
 import Spells from './Spells'
@@ -11,19 +11,31 @@ function Homepage() {
         setIsVisible(!isVisible)
     }
 
+    const [background, setBackground] = useState([])
+    useEffect(() => {
+        (async () => {
+              let req = await fetch('https://api.pathfinder2.fr/v1/pf2/background',{
+              headers:{Authorization:'da468b89-2bf8-4e2b-a939-79c6e6ef25ce'}
+              }
+              )
+              let res = await req.json()  
+              setBackground(res.results)
+          })()
+      }, []) 
+
     return (
         <>
         <div>
-            <h1>Header</h1>
+            <h1>Headers</h1>
         </div> 
         <button className="char-window-button" onClick={handleCharWindClick}>{isVisible ? 'Hide Character' : 'Show Character'}</button>
         {isVisible ? <CharacterWindow /> : null}
         <div>
             <Class />
         </div>
-        <div>
-            <Background />
-        </div> 
+        {background.map((element) => {
+            <Background element={element} />
+        })}
         <div>
             <Equipment />
         </div> 
