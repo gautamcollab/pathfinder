3a4006a6aeb696fdb9761ae8ceed6ea0a68a3b09
Gautam Sinha <gautam.sinha@live.com>
2022-04-18 13:13:43 -0400

    fetch request

--- src/Components/ClassContainer.js
+++ src/Components/ClassContainer.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import Class from "./Class";
 
 function ClassContainer(){
+    const [charClass, setCharClass] = useState([])
+
+
+    const request = async () => {
+        let req = await fetch('https://api.pathfinder2.fr/v1/pf2/class',{
+        headers:{Authorization:'da468b89-2bf8-4e2b-a939-79c6e6ef25ce'}
+        }
+        )
+        let res = await req.json()  
+        console.log(res)
+        setCharClass(res)
+    } 
+    useEffect(request,[])
+
+
     return (
-        <ul> Classes </ul>
+        <Class />
     );
 }
 
