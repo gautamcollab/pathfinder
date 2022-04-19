1eba9e06bbad0123e69c6014408f189e88d0461c
Gautam Sinha <gautam.sinha@live.com>
2022-04-19 13:58:37 -0400

    navbar update

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,49 +1,10 @@
-import { useState } from 'react'
-import Class from './Class'
-import BackgroundContainer from './BackgroundContainer'
 import React from 'react'
-import ClassContainer from './ClassContainer'
-import Spells from './Spells'
-import CharacterWindow from './CharacterWindow'
-import Equipment from './Equipment'
+import NavBar from './NavBar'
 
 function Homepage() {
-    const [isCharVisible, setCharVisible] = useState(false)
-    const [isBgVisible, setBgVisible] = useState(false)
-
-    const handleCharWindClick = () => {
-        setCharVisible(!isCharVisible)
-    }
-    const handleBgStoryClick = () => {
-        setBgVisible(!isBgVisible)
-    }
-
     return (
-        <>
-        <div>
-            <h1>Headers</h1>
-        </div> 
-        <div>
-            <button className="char-window-button" onClick={handleCharWindClick}>{isCharVisible ? 'Hide Character' : 'Show Character'}</button>
-            {isCharVisible ? <CharacterWindow /> : null}
-        </div>
-        <div>
-            <button>Classes</button> 
-        </div>
-        <div>
-            <button onClick={handleBgStoryClick}>Background Stories</button>
-            {isBgVisible ? <BackgroundContainer /> : null}
-        </div>
-        <div>
-            <Equipment />
-        </div> 
-        <div>
-            <Spells />
-        </div>
-        <div>Light Mode/Dark Mode</div> 
-        </>
+       <NavBar />
     )
-
 }
 
 
