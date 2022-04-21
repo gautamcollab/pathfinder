399fc3f632bf1f51bc81869ba609e4533f548f5a
John Kim <joki9448@gmail.com>
2022-04-21 08:14:58 -0400

    started name form

---
+++ NameForm.js
@@ -0,0 +1,10 @@
+function NameForm() {
+    return (
+        <div>
+            <input type="text" placeholder="What is your name?"></input>
+            <button></button>
+        </div>
+    )
+}
+
+export default NameForm;
