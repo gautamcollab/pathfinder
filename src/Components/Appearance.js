112ab1117671e3ca187e7aa1f5aaa1e231f1b72f
John Kim <joki9448@gmail.com>
2022-04-21 21:58:40 -0400

    replaced broken images

--- src/Components/Appearance.js
+++ src/Components/Appearance.js
@@ -4,26 +4,26 @@ function Appearance() {
     // console.log(imageData)
     return (
         <div className="model-container">
-            <img src={imageData.Char1} alt="alchemist" width="500" height="500"></img>
-            <img src={imageData.Char2} alt="barbarian" width="500" height="500"></img>
-            <img src={imageData.Char3} alt="battlemage" width="500" height="500"></img>
-            <img src={imageData.Char4} alt="champion" width="500" height="500"></img>
-            <img src={imageData.Char5} alt="cleric" width="500" height="500"></img>
-            <img src={imageData.Char6} alt="creature" width="500" height="500"></img>
-            <img src={imageData.Char7} alt="dragon" width="500" height="500"></img>
-            <img src={imageData.Char8} alt="dragonguy" width="500" height="500"></img>
-            <img src={imageData.Char9} alt="investigator" width="500" height="500"></img>
-            <img src={imageData.Char10} alt="magus" width="500" height="500"></img>
-            <img src={imageData.Char11} alt="oracle" width="500" height="500"></img>
-            <img src={imageData.Char12} alt="pumpkin" width="500" height="500"></img>
-            <img src={imageData.Char13} alt="ranger" width="500" height="500"></img>
-            <img src={imageData.Char14} alt="rogue" width="500" height="500"></img>
-            <img src={imageData.Char15} alt="skeleton" width="500" height="500"></img>
-            <img src={imageData.Char16} alt="sorceress" width="500" height="500"></img>
-            <img src={imageData.Char17} alt="troll" width="500" height="500"></img>
-            <img src={imageData.Char18} alt="werewolf" width="500" height="500"></img>
-            <img src={imageData.Char19} alt="witch" width="500" height="500"></img>
-            <img src={imageData.Char20} alt="wizard" width="500" height="500"></img>
+            <img className="model" src={imageData.Char1} alt="alchemist" width="500" height="500"></img>
+            <img className="model" src={imageData.Char2} alt="barbarian" width="500" height="500"></img>
+            <img className="model" src={imageData.Char3} alt="battlemage" width="500" height="500"></img>
+            <img className="model" src={imageData.Char4} alt="champion" width="500" height="500"></img>
+            <img className="model" src={imageData.Char5} alt="cleric" width="500" height="500"></img>
+            <img className="model" src={imageData.Char6} alt="creature" width="500" height="500"></img>
+            <img className="model" src={imageData.Char7} alt="dragon" width="500" height="500"></img>
+            <img className="model" src={imageData.Char8} alt="dragonguy" width="500" height="500"></img>
+            <img className="model" src={imageData.Char9} alt="investigator" width="500" height="500"></img>
+            <img className="model" src={imageData.Char10} alt="magus" width="500" height="500"></img>
+            <img className="model" src={imageData.Char11} alt="oracle" width="500" height="500"></img>
+            <img className="model" src={imageData.Char12} alt="pumpkin" width="500" height="500"></img>
+            <img className="model" src={imageData.Char13} alt="ranger" width="500" height="500"></img>
+            <img className="model" src={imageData.Char14} alt="rogue" width="500" height="500"></img>
+            <img className="model" src={imageData.Char15} alt="skeleton" width="500" height="500"></img>
+            <img className="model" src={imageData.Char16} alt="sorceress" width="500" height="500"></img>
+            <img className="model" src={imageData.Char17} alt="troll" width="500" height="500"></img>
+            <img className="model" src={imageData.Char18} alt="werewolf" width="500" height="500"></img>
+            <img className="model" src={imageData.Char19} alt="witch" width="500" height="500"></img>
+            <img className="model" src={imageData.Char20} alt="wizard" width="500" height="500"></img>
         </div>
     )
 }
